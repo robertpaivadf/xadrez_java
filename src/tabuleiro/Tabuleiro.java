399295ca6235793a9f47c1e2e9986daee0313b5e
Robert Paiva <robertpaiva.df@gmail.com>
2022-09-06 15:41:17 -0300

    Colocar peças no tabuleiro

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -36,5 +36,12 @@ public class Tabuleiro {
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 	
+	public void colocarPeca(Peca peca, Posicao posicao) {
+		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
+		peca.posicao = posicao;
+	}
+	
+	
+	
 	
 }
