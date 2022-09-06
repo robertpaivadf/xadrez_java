91b3e7e4c337cadb777d08011a7052750e57c70f
Robert Paiva <robertpaiva.df@gmail.com>
2022-09-06 18:21:13 -0300

    Add cor nas paças do tabuleiro

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,9 +1,17 @@
 package application;
 
+import xadrez.Cor;
 import xadrez.PecaXadrez;
 
 public class UI {
 
+	//COR DA FONTE
+	public static final String ANSI_WHITE = "\u001B[37m";
+	public static final String ANSI_BLACK = "\u001B[33m";
+	//COR DO FUNDO	
+	public static final String ANSI_RESET = "\u001B[0m";
+	
+	
 	public static void mostrarTabuleiro(PecaXadrez[][] pecas) {
 		for(int i=0;i<pecas.length; i++) {
 			System.out.print((8-i) + " ");
@@ -20,7 +28,13 @@ public class UI {
 			System.out.print("-");
 		}
 		else {
-			System.out.print(peca);
+			if(peca.getCor() == Cor.BRANCO) {
+				System.out.print(ANSI_WHITE + peca + ANSI_RESET);
+			}
+			else {
+				System.out.print(ANSI_BLACK + peca + ANSI_RESET);
+			}
+				
 		}
 		System.out.print(" ");
 	}
