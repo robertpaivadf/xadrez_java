fa0df1572f49892a7bf4a0826e804def23136a96
Robert Paiva <robertpaiva.df@gmail.com>
2022-09-06 15:02:25 -0300

    Classes Tabuleiro e Peca

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,13 @@
 package application;
 
 import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Posicao pos = new Posicao(3, 5);
-		
-		System.out.println(pos);
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 		
 	}
 
