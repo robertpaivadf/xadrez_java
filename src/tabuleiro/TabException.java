c0d166010fd8d2b0be42e5869ad8b299c2ff3a45
Robert Paiva <robertpaiva.df@gmail.com>
2022-09-06 16:15:59 -0300

    Add controle de exceção no tabuleiro

---
+++ src/tabuleiro/TabException.java
@@ -0,0 +1,10 @@
+package tabuleiro;
+
+public class TabException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public TabException(String msg) {
+		super(msg);
+	}
+	
+}
--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -24,7 +24,7 @@ public class PartidaXadrez {
 		return mat;
 	}
 	
-	private void iniciarPartida() {
+	private void iniciarPartida() {		
 		tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.BRANCO), new Posicao(2, 1));
 		tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.PRETO), new Posicao(0, 4));
 		tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.BRANCO), new Posicao(7, 4));
